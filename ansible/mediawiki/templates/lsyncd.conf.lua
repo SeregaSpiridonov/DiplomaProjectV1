8fe4627149201044d8f451cb065b9b8bc012ad65
Serega Spiridonov <letuchline@mail.ru>
2024-12-24 02:29:08 +0300

    Add lsyncd(beta)

---
+++ ansible/mediawiki/templates/lsyncd.conf.lua
@@ -0,0 +1,21 @@
+settings {
+    logfile = "/var/log/lsyncd.log",
+    statusFile = "/var/log/lsyncd.stat",
+    statusInterval = 10, --[[интервал обновления файла lsyncd.status в секундах]]
+    insist = true,
+    nodaemon = false,
+    maxProcesses = 1,
+}
+
+sync {
+    default.rsyncssh,
+    source="/var/www/{{ app_ver }}", --[[каталог-источник для синхронизации]]
+    host = "root@{{ &&&&!!!! }}",
+    targetdir="/var/www/{{ app_ver }}", --[[целевой каталог на сервере, заданный в параметре host]]
+    delete = 'running',
+    --delay = 20,
+    rsync = {
+        _extra = { "-ausS" },
+        update = true,
+    }
+}
